ccd08f8392fb0d90eedfcfdb0f054cbca559e25a
admkts01 <s25707@pjwstk.edu.pl>
2024-10-22 13:16:33 +0200

    Adam classes bs

---
+++ VetClinic/VetClinic/Appointment.cs
@@ -0,0 +1,22 @@
+﻿namespace VetClinic;
+
+public class Appointment :StoredObject<Appointment>,IIdentifiable
+{
+    public int Id { get; set; }
+    public DateTime DateTime { get; set; }
+    public int Price { get; set; }
+    
+    public Appointment() {}
+
+    public Appointment(DateTime dateTime, int price)
+    {
+        DateTime = dateTime;
+        Price = price;
+        AddToExtent(this);
+    }
+    
+    public override string ToString()
+    {
+        return $"Id={Id}, DateTime={DateTime}, Price={Price}";
+    }
+}
