5547c234c31d4833e99ecd2dc16c55cd2dc53036
Natalia <s25774@pjwstk.edu.pl>
2024-10-22 13:08:59 +0200

    Natalia-Classes

---
+++ VetClinic/VetClinic/Medication.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace VetClinic
+{
+
+    public enum Form { pill, injection, cream, powder }
+
+    public class Medication : StoredObject<Medication>, IIdentifiable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Form Form { get; set; }
+
+        public Medication() { }
+        public Medication(string name, Form form)
+        {
+
+            Name = name;
+            Form = form;
+            AddToExtent(this);
+        }
+
+
+        public override string ToString()
+        {
+            return $"Id={Id}, Name={Name}, Form={Form}";
+        }
+    }
+}
---
+++ VetClinic/VetClinic/Prescription.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace VetClinic
+{
+    public class Prescription:StoredObject<Prescription>, IIdentifiable
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Prescription() { }
+
+        public Prescription( DateTime startDate, DateTime endDate)
+        {
+           
+            StartDate = startDate;
+            EndDate = endDate;
+            AddToExtent(this);
+
+        }
+
+        public override string ToString()
+        {
+            return $"Id={Id}, StartDate={StartDate}, EndDate={EndDate}";
+        }
+
+    }
+}
