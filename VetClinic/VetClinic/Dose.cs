96273fbb974f74e6029c03f5eaa63406bb88cb0c
Natalia <s25774@pjwstk.edu.pl>
2024-11-03 14:51:04 +0100

    Natalia Classes edit
    Classes Client, Dose, Medication and Prescription have been fully edited.

--- VetClinic/VetClinic/Dose.cs
+++ VetClinic/VetClinic/Dose.cs
@@ -1,23 +1,45 @@
-﻿namespace VetClinic;
-
-public class Dose : StoredObject<Dose>, IIdentifiable
+﻿namespace VetClinic
 {
-    public int Id { get; set; }
-    public string Description { get; set; }
-    public double Amount { get; set; }
-
-    public Dose() {}
-
-    public Dose(string description, double amount)
+    public class Dose : StoredObject<Dose>, IIdentifiable
     {
-        Description = description;
-        Amount = amount;
-        AddToExtent(this);
-    }
+        public int Id { get; set; }
 
-    public override string ToString()
-    {
-        return $"Description={Description}, Amount={Amount}";
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Description can't be empty.");
+                _description = value;
+            }
+        }
+
+        private double _amount;
+        public double Amount
+        {
+            get => _amount;
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Amount must be a positive value.");
+                _amount = value;
+            }
+        }
+
+        public Dose() { }
+
+        public Dose(string description, double amount)
+        {
+            Description = description;
+            Amount = amount;
+            AddToExtent(this);
+        }
+
+        public override string ToString()
+        {
+            return $"Description={Description}, Amount={Amount}";
+        }
     }
-    
 }
